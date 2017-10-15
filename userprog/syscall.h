02d4e8c2618d81c5f62acba1198b9d2b928bb0a6
fish <luk1684tw@gmail.com>
2017-10-15 17:29:29 +0800

    [update]finish with a report

--- userprog/syscall.h
+++ userprog/syscall.h
@@ -27,8 +27,8 @@
 #define SC_Open		6
 #define SC_Read		7
 #define SC_Write	8
-#define SC_Seek         9
 #define SC_Close	10
+#define SC_Seek          9
 #define SC_ThreadFork	11
 #define SC_ThreadYield	12
 #define SC_ExecV	13
